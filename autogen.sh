3c88ef44720be9fc465a8899964f3ad5596bd6d1
Paul Querna <chip@outoforder.cc>
2004-09-19 03:01:02 +0000

    mod_highlight build fixes. it now compiles on solaris

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # autogen.sh - generates configure using the autotools
 # $Id: autogen.sh,v 1.1 2004/03/04 08:12:13 firechipmunk Exp $
-export WANT_AUTOCONF=2.5
+#export WANT_AUTOCONF=2.5
 libtoolize --force --copy
-#libtoolize14  --force --copy
 aclocal -I m4
 autoheader
 automake --add-missing --copy --foreign
 autoconf
 rm -rf autom4te.cache
+cd colorer
+libtoolize --force --copy
+aclocal -I m4
+autoheader
+automake --add-missing --copy --foreign
+autoconf
+rm -rf autom4te.cache
+
